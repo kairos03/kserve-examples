7d34ea4702e3e0bd045d7804b7767c927fc7ab31
kairos03 <kairos9603@gmail.com>
2022-05-06 12:36:39 +0900

    add prediction code

--- examples/custom-classifier/README.md
+++ examples/custom-classifier/README.md
@@ -18,3 +18,14 @@ kubectl cp cifar model-store-pod:/models -c model-store
 ls /model
 ```
 
+```
+kubectl apply -f inferenceservice.yaml
+```
+
+```
+kubectl get isvc
+
+NAME           URL                                       READY   PREV   LATEST   PREVROLLEDOUTREVISION   LATESTREADYREVISION                    AGE
+onnx-cifar10   http://onnx-cifar10.omnious.example.com   True           100                              onnx-cifar10-predictor-default-00001   1m
+```
+
--- examples/custom-classifier/prepare_model.py
+++ examples/custom-classifier/prepare_model.py
@@ -1,19 +1,25 @@
 import torch
-model = torch.hub.load("chenyaofo/pytorch-cifar-models", "cifar10_resnet20", pretrained=True)
+
+model = torch.hub.load(
+    "chenyaofo/pytorch-cifar-models", "cifar10_resnet20", pretrained=True
+)
 
 # 모델에 대한 입력값
 x = torch.randn(4, 3, 32, 32, requires_grad=True)
 torch_out = model(x)
 
 # 모델 변환
-torch.onnx.export(model,                     # 실행될 모델
-                  x,                         # 모델 입력값 (튜플 또는 여러 입력값들도 가능)
-                  "model/classifier/1/model.onnx",   # 모델 저장 경로 (파일 또는 파일과 유사한 객체 모두 가능)
-                  export_params=True,        # 모델 파일 안에 학습된 모델 가중치를 저장할지의 여부
-                  opset_version=10,          # 모델을 변환할 때 사용할 ONNX 버전
-                  do_constant_folding=True,  # 최적화시 상수폴딩을 사용할지의 여부
-                  input_names = ['input'],   # 모델의 입력값을 가리키는 이름
-                  output_names = ['output'], # 모델의 출력값을 가리키는 이름
-                  dynamic_axes={'input' : {0 : 'batch_size'},    # 가변적인 길이를 가진 차원
-                                'output' : {0 : 'batch_size'}}
-                )
+torch.onnx.export(
+    model,  # 실행될 모델
+    x,  # 모델 입력값 (튜플 또는 여러 입력값들도 가능)
+    "model/classifier/1/model.onnx",  # 모델 저장 경로 (파일 또는 파일과 유사한 객체 모두 가능)
+    export_params=True,  # 모델 파일 안에 학습된 모델 가중치를 저장할지의 여부
+    opset_version=10,  # 모델을 변환할 때 사용할 ONNX 버전
+    do_constant_folding=True,  # 최적화시 상수폴딩을 사용할지의 여부
+    input_names=["input"],  # 모델의 입력값을 가리키는 이름
+    output_names=["output"],  # 모델의 출력값을 가리키는 이름
+    dynamic_axes={
+        "input": {0: "batch_size"},  # 가변적인 길이를 가진 차원
+        "output": {0: "batch_size"},
+    },
+)
